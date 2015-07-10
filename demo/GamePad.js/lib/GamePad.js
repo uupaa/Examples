6f0e3d136df4e1a2a0f8c4d5307b90cc096aa31f
uupaa <uupaa.js@gmail.com>
2015-07-10 13:27:53 +0900

    update GamePad demo

--- demo/GamePad.js/lib/GamePad.js
+++ demo/GamePad.js/lib/GamePad.js
@@ -16,87 +16,117 @@ var Catalog = global["WebModule"]["GamePadCatalog"];
 
 // --- define / local variables ----------------------------
 var PADS = Catalog["PADS"];
+var NOP = function() {};
 
 // --- class / interfaces ----------------------------------
-function GamePad(id) { // @arg GamePadIDString
-    var gamepadList = navigator.getGamepads();
+function GamePad(gamepad) { // @arg Gamepad
+    this._player  = gamepad["index"];
+    this._type    = PADS[gamepad["id"]];
+    this._last    = {}; // last value
+    this._edge    = {}; // edge value
+    this._value   = {}; // current value
+    this._handler = GamePad[this._type];
+    // --- setup keycode handler ---
+    this._keyCode       = null;
+    this._oncontextmenu = null;
+    this._onkeydown     = null;
+    this._onkeyup       = null;
 
-    this._padIndex = 0;
-    for (var i = 0, iz = gamepadList.length; i < iz; ++i) {
-        if (gamepadList[i]["id"] === id) {
-            this._id            = id;
-            this._type          = PADS[this._id][1] || "GAMEPAD";
-            this._last          = null; // last value
-            this._edge          = null; // edge status
-            this._value         = {};   // current value
-            this._handler       = GamePad[PADS[this._id][0]].bind(this); // assign handler
-            // key buffer: 10min * 60sec * 60frame * 4bytes = 140KB
-            this._buffer        = new Uint32Array(10 * 60 * 60);
-            this._bufferCursor  = 0;
-            this._padIndex      = i;
-            this["scan"]();
-            break;
-        }
-    }
+    this._handler["setup"].call(this);
+    this["scan"](gamepad);
 }
 
 GamePad["repository"] = "https://github.com/uupaa/GamePad.js";
 GamePad["prototype"] = Object.create(GamePad, {
-    "constructor":  { "value": GamePad                              }, // new GamePad(id:GamePadIDString):GamePad
-    "id":           { "get":   function()  { return this._id;     } }, // GamePad#id:GamePadIDString
+    "constructor":  { "value": GamePad                              }, // new GamePad(gamepad:Gamepad):GamePad
     "type":         { "get":   function()  { return this._type;   } }, // GamePad#type:GamePadTypeString
-    "scan":         { "value": GamePad_scan                         }, // GamePad#scan():this
-    "edge":         { "get":   function()  { return this._edge;   } }, // GamePad#edge:GamePadEdgeObject
+    "scan":         { "value": GamePad_scan                         }, // GamePad#scan(gamepad:Gamepad):void
+    "edge":         { "get":   function()  { return this._edge;   } }, // GamePad#edge:GamePadEdgeStateObject
     "value":        { "get":   function()  { return this._value;  } }, // GamePad#value:GamePadValueObject
-    "buffer":       { "get":   function()  { return this._buffer; } }, // GamePad#buffer:Object
-    "connected":    { "get":   GamePad_connected                    }, // GamePad#connected:Boolean
+    "disconnect":   { "value": GamePad_disconnect                   }, // GamePad#disconnect():void
 });
-GamePad["NexusPlayer"] = GamePad_NexusPlayer;
+GamePad["NexusPlayer"] = {
+    "scan":     GamePad_NexusPlayer_scan,
+    "setup":    NOP,
+    "tearDown": NOP
+};
+GamePad["FC801"] = {
+    "scan":     GamePad_FC801_scan,
+    "setup":    NOP,
+    "tearDown": NOP
+};
+GamePad["SFC801"] = {
+    "scan":     GamePad_SFC801_scan,
+    "setup":    NOP,
+    "tearDown": NOP
+};
+GamePad["FC30"] = {
+    "scan":     GamePad_FC30_scan,
+    "setup":    GamePad_FC30_setup,
+    "tearDown": GamePad_FC30_tearDown
+};
 
 // --- implements ------------------------------------------
-function GamePad_scan() { // @ret this
-    var gamepad = navigator["getGamepads"]()[this._padIndex];
+function GamePad_scan(pad) { // @arg Gamepad
+    var last = this._value;
+    var curt = this._handler["scan"].call(this, pad["buttons"], pad["axes"]); // { U,R,D,L,A,B,X,Y,L1,R1,L2,R2,L3,R3,LSX,LSY,RSX,RSY,U32 }
 
-    this._buffer[this._bufferCursor++] = this._handler(gamepad);
-
-    if (this._bufferCursor >= this._buffer.length) {
-        this._bufferCursor = 0;
-    }
-    return this;
+    this._last  = last;
+    this._value = curt;
+    this._edge  = {
+        // --- D-PAD ---
+        "U":    last["U"]   !== curt["U"],
+        "R":    last["R"]   !== curt["R"],
+        "D":    last["D"]   !== curt["D"],
+        "L":    last["L"]   !== curt["L"],
+        // --- DIGITAL ---
+        "A":    last["A"]   !== curt["A"],
+        "B":    last["B"]   !== curt["B"],
+        "X":    last["X"]   !== curt["X"],
+        "Y":    last["Y"]   !== curt["Y"],
+        "L1":   last["L1"]  !== curt["L1"],
+        "R1":   last["R1"]  !== curt["R1"],
+        "L3":   last["L3"]  !== curt["L3"],
+        "R3":   last["R3"]  !== curt["R3"],
+        // --- ANALOG ---
+        "LSX":  last["LSX"] !== curt["LSX"],
+        "LSY":  last["LSY"] !== curt["LSY"],
+        "RSX":  last["RSX"] !== curt["RSX"],
+        "RSY":  last["RSY"] !== curt["RSY"],
+        "L2":   last["L2"]  !== curt["L2"],
+        "R2":   last["R2"]  !== curt["R2"],
+    };
 }
 
-function GamePad_connected() { // @ret Boolean
-    var gamepad = navigator["getGamepads"]()[this._padIndex];
-
-    return (gamepad || {})["connected"];
+function GamePad_disconnect() {
+    this._handler["tearDown"].call(this);
 }
 
-function GamePad_NexusPlayer(gamepad) { // @arg GamePadObject
-                                        // @ret UINT32
-    var padButtons = gamepad["buttons"];
-    var padAxes    = gamepad["axes"];
-
+// === Nexus Player ========================================
+function GamePad_NexusPlayer_scan(buttons, // @arg GamePadButtonObjects
+                                  axes) {  // @arg GamePadAxesObjects
+                                           // @ret GamePadValueObject
     // --- D-PAD ---
     var U   = 0;
     var R   = 0;
     var D   = 0;
     var L   = 0;
-    // --- BUTTON ---
-    var A   = padButtons[ 0]["pressed"] ? 1 : 0; // 0 or 1
-    var B   = padButtons[ 1]["pressed"] ? 1 : 0; // 0 or 1
-    var X   = padButtons[ 3]["pressed"] ? 1 : 0; // 0 or 1
-    var Y   = padButtons[ 4]["pressed"] ? 1 : 0; // 0 or 1
-    var L1  = padButtons[ 6]["pressed"] ? 1 : 0; // 0 or 1
-    var R1  = padButtons[ 7]["pressed"] ? 1 : 0; // 0 or 1
-    var SL  = padButtons[13]["pressed"] ? 1 : 0; // 0 or 1 | STICK L PRESSED
-    var SR  = padButtons[14]["pressed"] ? 1 : 0; // 0 or 1 | STICK R PRESSED
-    // --- AXIS ---
-    var SLX = (padAxes[0] + 1) * 7 | 0; // 0 ... 7 ... 14 | STICK L X
-    var SLY = (padAxes[1] + 1) * 7 | 0; // 0 ... 7 ... 14 | STICK L Y
-    var SRX = (padAxes[2] + 1) * 7 | 0; // 0 ... 7 ... 14 | STICK R X
-    var SRY = (padAxes[5] + 1) * 7 | 0; // 0 ... 7 ... 14 | STICK R Y
-    var L2  = (padAxes[3] + 1) * 1 | 0; // 0 ... 2 | L2
-    var R2  = (padAxes[4] + 1) * 1 | 0; // 0 ... 2 | R2
+    // --- DIGITAL ---
+    var A   = buttons[ 0]["pressed"] ? 1 : 0; // 0 or 1
+    var B   = buttons[ 1]["pressed"] ? 1 : 0; // 0 or 1
+    var X   = buttons[ 3]["pressed"] ? 1 : 0; // 0 or 1
+    var Y   = buttons[ 4]["pressed"] ? 1 : 0; // 0 or 1
+    var L1  = buttons[ 6]["pressed"] ? 1 : 0; // 0 or 1
+    var R1  = buttons[ 7]["pressed"] ? 1 : 0; // 0 or 1
+    var L3  = buttons[13]["pressed"] ? 1 : 0; // 0 or 1 | L STICK PRESSED
+    var R3  = buttons[14]["pressed"] ? 1 : 0; // 0 or 1 | L STICK PRESSED
+    // --- ANALOG ---
+    var LSX = (axes[0] + 1) * 7 | 0; // 0 ... 7 ... 14 | L STICK X
+    var LSY = (axes[1] + 1) * 7 | 0; // 0 ... 7 ... 14 | L STICK Y
+    var RSX = (axes[2] + 1) * 7 | 0; // 0 ... 7 ... 14 | R STICK X
+    var RSY = (axes[5] + 1) * 7 | 0; // 0 ... 7 ... 14 | R STICK Y
+    var L2  = (axes[3] + 1) * 1 | 0; // 0 ... 2 | L2
+    var R2  = (axes[4] + 1) * 1 | 0; // 0 ... 2 | R2
 
     // --- D-PAD ---
     //  axes[9] is D_PAD input
@@ -110,7 +140,7 @@ function GamePad_NexusPlayer(gamepad) { // @arg GamePadObject
     //      R:      -0.42 + 1.0 = 0.58
     //      U+R:    -0.71 + 1.0 = 0.29
     //      U:      -1.00 + 1.0 = 0.00
-    var D_PAD = padAxes[9] + 1; // 1 is offset
+    var D_PAD = axes[9] + 1; // 1 is offset
 
     if (D_PAD > 2.00 + 0.05) { /*NEUTRAL*/ } else
     if (D_PAD > 2.00 - 0.05) { U = L = 1;  } else
@@ -122,62 +152,260 @@ function GamePad_NexusPlayer(gamepad) { // @arg GamePadObject
     if (D_PAD > 0.29 - 0.05) { R = U = 1;  } else
     if (D_PAD > 0.00 - 0.05) {     U = 1;  }
 
-    this._last = this._value; // ref
-    this._value = {
+    return {
         // --- D-PAD ---
         "U":    U,
         "R":    R,
         "D":    D,
         "L":    L,
-        // --- button ---
+        // --- DIGITAL ---
         "A":    A,
         "B":    B,
         "X":    X,
         "Y":    Y,
         "L1":   L1,
         "R1":   R1,
-        "SL":   SL,
-        "SR":   SR,
-        // --- axis ---
-        "SLX":  SLX,
-        "SLY":  SLY,
-        "SRX":  SRX,
-        "SRY":  SRY,
+        "L3":   L3,
+        "R3":   R3,
+        // --- ANALOG ---
+        "LSX":  LSX,
+        "LSY":  LSY,
+        "RSX":  RSX,
+        "RSY":  RSY,
         "L2":   L2,
         "R2":   R2,
+        // --- key buffer ---
+        // |FEDCBA9876543210|FEDCBA9876543210|
+        // |----            |                | LSX, Left  Stick X (0 .. 7 .. 14) NEUTRAL is 7
+        // |    ----        |                | LSY, Left  Stick Y (0 .. 7 .. 14) NEUTRAL is 7
+        // |        ----    |                | RSX, Right Stick X (0 .. 7 .. 14) NEUTRAL is 7
+        // |            ----|                | RSY, Right Stick Y (0 .. 7 .. 14) NEUTRAL is 7
+        // |                |-               | L1                 (0 or 1)
+        // |                | -              | R1                 (0 or 1)
+        // |                |  --            | L2                 (0 .. 2) NEUTRAL is 0
+        // |                |    --          | R2                 (0 .. 2) NEUTRAL is 0
+        // |                |      -         | L3                 (0 or 1)
+        // |                |       -        | R3                 (0 or 1)
+        // |                |        -       | A                  (0 or 1)
+        // |                |         -      | B                  (0 or 1)
+        // |                |          -     | X                  (0 or 1)
+        // |                |           -    | Y                  (0 or 1)
+        // |                |            -   | U,  D-PAD Up       (0 or 1)
+        // |                |             -  | R,  D-PAD Right    (0 or 1)
+        // |                |              - | D,  D-PAD Down     (0 or 1)
+        // |                |               -| L,  D-PAD Left     (0 or 1)
+        "U32":  (LSX << 28 | LSY << 24 | RSX << 20 | RSY << 16 |
+                 L1  << 15 | R1  << 14 | L2  << 12 | R2  << 10 | L3 << 9 | R3 << 8 |
+                 A   <<  7 | B   <<  6 | X   <<  5 | Y   <<  4 |
+                 U   <<  3 | R   <<  2 | D   <<  1 | L) >>> 0
     };
+}
 
-    var last = this._last;
+// === FC801 ===============================================
+function GamePad_FC801_scan(buttons, // @arg GamePadButtonObjects
+                            axes) {  // @arg GamePadAxesObjects
+                                     // @ret GamePadValueObject
+    // --- D-PAD ---
+    var U   = axes[1] === -1 ? 1 : 0;
+    var R   = axes[0] ===  1 ? 1 : 0;
+    var D   = axes[1] ===  1 ? 1 : 0;
+    var L   = axes[0] === -1 ? 1 : 0;
+    // --- DIGITAL ---
+    var A   = buttons[ 0]["pressed"] ? 1 : 0; // 0 or 1
+    var B   = buttons[ 1]["pressed"] ? 1 : 0; // 0 or 1
+    var X   = buttons[ 2]["pressed"] ? 1 : 0; // 0 or 1
+    var Y   = buttons[ 3]["pressed"] ? 1 : 0; // 0 or 1
+    var L1  = buttons[ 4]["pressed"] ? 1 : 0; // 0 or 1
+    var R1  = buttons[ 5]["pressed"] ? 1 : 0; // 0 or 1
+    var L3  = buttons[ 6]["pressed"] ? 1 : 0; // 0 or 1 // START
+    var R3  = buttons[ 7]["pressed"] ? 1 : 0; // 0 or 1 // SELECT
+    // --- ANALOG ---
+    var LSX = 7;
+    var LSY = 7;
+    var RSX = 7;
+    var RSY = 7;
+    var L2  = 0;
+    var R2  = 0;
 
-    this._edge = {
+    return {
         // --- D-PAD ---
-        "U":    last["U"]   !== U,
-        "R":    last["R"]   !== R,
-        "D":    last["D"]   !== D,
-        "L":    last["L"]   !== L,
-        // --- button ---
-        "A":    last["A"]   !== A,
-        "B":    last["B"]   !== B,
-        "X":    last["X"]   !== X,
-        "Y":    last["Y"]   !== Y,
-        "L1":   last["L1"]  !== L1,
-        "R1":   last["R1"]  !== R1,
-        "SL":   last["SL"]  !== SL,
-        "SR":   last["SR"]  !== SR,
-        // --- axis ---
-        "SLX":  last["SLX"] !== SLX,
-        "SLY":  last["SLY"] !== SLY,
-        "SRX":  last["SRX"] !== SRX,
-        "SRY":  last["SRY"] !== SRY,
-        "L2":   last["L2"]  !== L2,
-        "R2":   last["R2"]  !== R2,
+        "U":    U,
+        "R":    R,
+        "D":    D,
+        "L":    L,
+        // --- DIGITAL ---
+        "A":    A,
+        "B":    B,
+        "X":    X,
+        "Y":    Y,
+        "L1":   L1,
+        "R1":   R1,
+        "L3":   L3,
+        "R3":   R3,
+        // --- ANALOG ---
+        "LSX":  LSX,
+        "LSY":  LSY,
+        "RSX":  RSX,
+        "RSY":  RSY,
+        "L2":   L2,
+        "R2":   R2,
+        // --- key buffer ---
+        "U32":  (LSX << 28 | LSY << 24 | RSX << 20 | RSY << 16 |
+                 L1  << 15 | R1  << 14 | L2  << 12 | R2  << 10 | L3 << 9 | R3 << 8 |
+                 A   <<  7 | B   <<  6 | X   <<  5 | Y   <<  4 |
+                 U   <<  3 | R   <<  2 | D   <<  1 | L) >>> 0
     };
+}
 
-    return (U   << 31 | R   << 30 | D   << 29 | L   << 28 |
-            A   << 27 | B   << 26 | X   << 25 | Y   << 24 |
-            L1  << 23 | R1  << 22 | SL  << 21 | SR  << 20 |
-            SLX << 16 | SLY << 12 | SRX <<  8 | SRY <<  4 |
-            L2  <<  2 | R2) >>> 0;
+// === SFC801 ===============================================
+function GamePad_SFC801_scan(buttons, // @arg GamePadButtonObjects
+                             axes) {  // @arg GamePadAxesObjects
+                                      // @ret GamePadValueObject
+    // --- D-PAD ---
+    var U   = axes[1] === -1 ? 1 : 0;
+    var R   = axes[0] ===  1 ? 1 : 0;
+    var D   = axes[1] ===  1 ? 1 : 0;
+    var L   = axes[0] === -1 ? 1 : 0;
+    // --- DIGITAL ---
+    var A   = buttons[ 1]["pressed"] ? 1 : 0; // 0 or 1
+    var B   = buttons[ 0]["pressed"] ? 1 : 0; // 0 or 1
+    var X   = buttons[ 3]["pressed"] ? 1 : 0; // 0 or 1
+    var Y   = buttons[ 2]["pressed"] ? 1 : 0; // 0 or 1
+    var L1  = buttons[ 4]["pressed"] ? 1 : 0; // 0 or 1
+    var R1  = buttons[ 5]["pressed"] ? 1 : 0; // 0 or 1
+    var L3  = buttons[ 6]["pressed"] ? 1 : 0; // 0 or 1 // START
+    var R3  = buttons[ 7]["pressed"] ? 1 : 0; // 0 or 1 // SELECT
+    // --- ANALOG ---
+    var LSX = 7;
+    var LSY = 7;
+    var RSX = 7;
+    var RSY = 7;
+    var L2  = 0;
+    var R2  = 0;
+
+    return {
+        // --- D-PAD ---
+        "U":    U,
+        "R":    R,
+        "D":    D,
+        "L":    L,
+        // --- DIGITAL ---
+        "A":    A,
+        "B":    B,
+        "X":    X,
+        "Y":    Y,
+        "L1":   L1,
+        "R1":   R1,
+        "L3":   L3,
+        "R3":   R3,
+        // --- ANALOG ---
+        "LSX":  LSX,
+        "LSY":  LSY,
+        "RSX":  RSX,
+        "RSY":  RSY,
+        "L2":   L2,
+        "R2":   R2,
+        // --- key buffer ---
+        "U32":  (LSX << 28 | LSY << 24 | RSX << 20 | RSY << 16 |
+                 L1  << 15 | R1  << 14 | L2  << 12 | R2  << 10 | L3 << 9 | R3 << 8 |
+                 A   <<  7 | B   <<  6 | X   <<  5 | Y   <<  4 |
+                 U   <<  3 | R   <<  2 | D   <<  1 | L) >>> 0
+    };
+}
+
+// === FC30 ================================================
+function GamePad_FC30_setup() {
+    this._keyCode       = { U: 0, R: 0, D: 0, L: 0 };
+
+    this._oncontextmenu = oncontextmenu.bind(this);
+    this._onkeydown     = onkeydown.bind(this);
+    this._onkeyup       = onkeyup.bind(this);
+
+    document.addEventListener("contextmenu", this._oncontextmenu); // Ignore the contextmenu event. Because B button firing it.
+    document.addEventListener("keydown", this._onkeydown);         // key code buffer. D-PAD fired keyboard event.
+    document.addEventListener("keyup", this._onkeyup);
+}
+
+function GamePad_FC30_tearDown() {
+    document.removeEventListener("contextmenu", this._oncontextmenu);
+    document.removeEventListener("keydown", this._onkeydown);
+    document.removeEventListener("keyup", this._onkeyup);
+}
+
+function oncontextmenu(event) {
+    event.preventDefault();
+}
+function onkeydown(event) {
+    switch (event.keyCode) {
+    case 38: this._keyCode.U = 1; break;
+    case 39: this._keyCode.R = 1; break;
+    case 40: this._keyCode.D = 1; break;
+    case 37: this._keyCode.L = 1; break;
+    default:
+             console.log(event.keyCode);
+    }
+}
+function onkeyup(event) {
+    switch (event.keyCode) {
+    case 38: this._keyCode.U = 0; break;
+    case 39: this._keyCode.R = 0; break;
+    case 40: this._keyCode.D = 0; break;
+    case 37: this._keyCode.L = 0; break;
+    }
+}
+
+function GamePad_FC30_scan(buttons) { // @arg GamePadButtonObjects
+                                      // @ret GamePadValueObject
+    // --- D-PAD ---
+    var U   = this._keyCode.U;
+    var R   = this._keyCode.R;
+    var D   = this._keyCode.D;
+    var L   = this._keyCode.L;
+    // --- DIGITAL ---
+    var A   = buttons[ 0]["pressed"] ? 1 : 0; // 0 or 1
+    var B   = buttons[ 1]["pressed"] ? 1 : 0; // 0 or 1
+    var X   = buttons[ 3]["pressed"] ? 1 : 0; // 0 or 1
+    var Y   = buttons[ 4]["pressed"] ? 1 : 0; // 0 or 1
+    var L1  = buttons[ 6]["pressed"] ? 1 : 0; // 0 or 1
+    var R1  = buttons[ 7]["pressed"] ? 1 : 0; // 0 or 1
+    var L3  = 0;
+    var R3  = 0;
+    // --- ANALOG ---
+    var LSX = 7;
+    var LSY = 7;
+    var RSX = 7;
+    var RSY = 7;
+    var L2  = 0;
+    var R2  = 0;
+
+    return {
+        // --- D-PAD ---
+        "U":    U,
+        "R":    R,
+        "D":    D,
+        "L":    L,
+        // --- DIGITAL ---
+        "A":    A,
+        "B":    B,
+        "X":    X,
+        "Y":    Y,
+        "L1":   L1,
+        "R1":   R1,
+        "L3":   L3,
+        "R3":   R3,
+        // --- ANALOG ---
+        "LSX":  LSX,
+        "LSY":  LSY,
+        "RSX":  RSX,
+        "RSY":  RSY,
+        "L2":   L2,
+        "R2":   R2,
+        // --- key buffer ---
+        "U32":  (LSX << 28 | LSY << 24 | RSX << 20 | RSY << 16 |
+                 L1  << 15 | R1  << 14 | L2  << 12 | R2  << 10 | L3 << 9 | R3 << 8 |
+                 A   <<  7 | B   <<  6 | X   <<  5 | Y   <<  4 |
+                 U   <<  3 | R   <<  2 | D   <<  1 | L) >>> 0
+    };
 }
 
 return GamePad; // return entity
--- demo/GamePad.js/lib/GamePadPlayer.js
+++ demo/GamePad.js/lib/GamePadPlayer.js
@@ -8,88 +8,32 @@ if (typeof module !== "undefined") {
 }
 return entity;
 
-})("GamePadPlayer", function moduleClosure(global) {
+})("GamePadPlayer", function moduleClosure() {
 "use strict";
 
 // --- dependency modules ----------------------------------
-var Catalog = global["WebModule"]["GamePadCatalog"];
-
 // --- define / local variables ----------------------------
-var PADS = Catalog["PADS"];
-var DEFAULT_STATUS = {
-        // --- D-PAD ---
-        "UP":   false,
-        "RIGHT":false,
-        "DOWN": false,
-        "LEFT": false,
-        // --- button ---
-        "A":    false,
-        "B":    false,
-        "X":    false,
-        "Y":    false,
-        "L":    false,
-        "R":    false,
-        "S1":   false,
-        "S2":   false,
-        // --- axis ---
-        "S1X":  0,
-        "S1Y":  0,
-        "S2X":  0,
-        "S2Y":  0,
-        "L2":   -1,
-        "R2":   -1,
-    };
-var DEFAULT_EDGE_STATUS = {
-        // --- D-PAD ---
-        "UP":   false,
-        "RIGHT":false,
-        "DOWN": false,
-        "LEFT": false,
-        // --- button ---
-        "A":    false,
-        "B":    false,
-        "X":    false,
-        "Y":    false,
-        "L":    false,
-        "R":    false,
-        "S1":   false,
-        "S2":   false,
-        // --- axis ---
-        "S1X":  false,
-        "S1Y":  false,
-        "S2X":  false,
-        "S2Y":  false,
-        "L2":   false,
-        "R2":   false,
-    };
-
 // --- class / interfaces ----------------------------------
-function GamePadPlayer(id,       // @arg GamePadIDString
-                       buffer) { // @arg Uint32Array
-    this._id            = id;
-    this._type          = PADS[this._id][1] || "GAMEPAD";
-    this._last          = null; // last value
+function GamePadPlayer(buffer) { // @arg Uint32Array
+    this._type          = "BufferPlayer";
     this._edge          = null; // edge status
     this._value         = {};   // current value
-    this._handler       = GamePadPlayer[PADS[this._id][0]].bind(this); // assign handler
     this._buffer        = buffer;
     this._bufferCursor  = 0;
 }
 
 GamePadPlayer["prototype"] = Object.create(GamePadPlayer, {
     "constructor":  { "value": GamePadPlayer                       }, // new GamePadPlayer(id:GamePadIDString, buffer:Uint32Array):GamePadPlayer
-    "id":           { "get":   function()  { return this._id;    } }, // GamePadPlayer#id:GamePadIDString
     "type":         { "get":   function()  { return this._type;  } }, // GamePadPlayer#type:GamePadTypeString
     "scan":         { "value": GamePadPlayer_scan                  }, // GamePadPlayer#scan():this
     "edge":         { "get":   function()  { return this._edge;  } }, // GamePadPlayer#edge:GamePadEdgeObject
     "value":        { "get":   function()  { return this._value; } }, // GamePadPlayer#value:GamePadValueObject
     "connected":    { "get":   function()  { return true;        } }, // GamePadPlayer#connected:Boolean
 });
-GamePadPlayer["NexusPlayer"] = GamePadPlayer_NexusPlayer;
 
 // --- implements ------------------------------------------
 function GamePadPlayer_scan() { // @ret this
-    this._handler(this._buffer[this._bufferCursor++] >>> 0);
+    _update.call(this, this._buffer[this._bufferCursor++] >>> 0, this._value);
 
     if (this._bufferCursor >= this._buffer.length) {
         this._bufferCursor = 0;
@@ -97,34 +41,51 @@ function GamePadPlayer_scan() { // @ret this
     return this;
 }
 
-function GamePadPlayer_NexusPlayer(u32) { // @arg Uint32
-
-    this._last = this._value; // ref
+function _update(u32, last) {
     this._value = {
+        // --- key buffer ---
+        // |FEDCBA9876543210|FEDCBA9876543210|
+        // |----            |                | LSX, Left  Stick X (0 .. 7 .. 14) NEUTRAL is 7
+        // |    ----        |                | LSY, Left  Stick Y (0 .. 7 .. 14) NEUTRAL is 7
+        // |        ----    |                | RSX, Right Stick X (0 .. 7 .. 14) NEUTRAL is 7
+        // |            ----|                | RSY, Right Stick Y (0 .. 7 .. 14) NEUTRAL is 7
+        // |                |-               | L1                 (0 or 1)
+        // |                | -              | R1                 (0 or 1)
+        // |                |  --            | L2                 (0 .. 2) NEUTRAL is 0
+        // |                |    --          | R2                 (0 .. 2) NEUTRAL is 0
+        // |                |      -         | L3                 (0 or 1)
+        // |                |       -        | R3                 (0 or 1)
+        // |                |        -       | A                  (0 or 1)
+        // |                |         -      | B                  (0 or 1)
+        // |                |          -     | X                  (0 or 1)
+        // |                |           -    | Y                  (0 or 1)
+        // |                |            -   | U,  D-PAD Up       (0 or 1)
+        // |                |             -  | R,  D-PAD Right    (0 or 1)
+        // |                |              - | D,  D-PAD Down     (0 or 1)
+        // |                |               -| L,  D-PAD Left     (0 or 1)
         // --- D-PAD ---
-        "U":    (u32 >>> 31) & 0x1,
-        "R":    (u32 >>> 30) & 0x1,
-        "D":    (u32 >>> 29) & 0x1,
-        "L":    (u32 >>> 28) & 0x1,
+        "U":    (u32 >>>  3) & 0x1,
+        "R":    (u32 >>>  2) & 0x1,
+        "D":    (u32 >>>  1) & 0x1,
+        "L":    (u32 >>>  0) & 0x1,
         // --- button ---
-        "A":    (u32 >>> 27) & 0x1,
-        "B":    (u32 >>> 26) & 0x1,
-        "X":    (u32 >>> 25) & 0x1,
-        "Y":    (u32 >>> 24) & 0x1,
-        "L1":   (u32 >>> 23) & 0x1,
-        "R1":   (u32 >>> 22) & 0x1,
-        "SL":   (u32 >>> 21) & 0x1,
-        "SR":   (u32 >>> 20) & 0x1,
+        "A":    (u32 >>>  7) & 0x1,
+        "B":    (u32 >>>  6) & 0x1,
+        "X":    (u32 >>>  5) & 0x1,
+        "Y":    (u32 >>>  4) & 0x1,
+        "L1":   (u32 >>> 15) & 0x1,
+        "R1":   (u32 >>> 14) & 0x1,
+        "L3":   (u32 >>>  9) & 0x1,
+        "R3":   (u32 >>>  8) & 0x1,
         // --- axis ---
-        "SLX":  (u32 >>> 16) & 0xf,
-        "SLY":  (u32 >>> 12) & 0xf,
-        "SRX":  (u32 >>>  8) & 0xf,
-        "SRY":  (u32 >>>  4) & 0xf,
-        "L2":   (u32 >>>  2) & 0x3,
-        "R2":   (u32 >>>  0) & 0x3,
+        "LSX":  (u32 >>> 28) & 0xf,
+        "LSY":  (u32 >>> 24) & 0xf,
+        "RSX":  (u32 >>> 20) & 0xf,
+        "RSY":  (u32 >>> 16) & 0xf,
+        "L2":   (u32 >>> 12) & 0x3,
+        "R2":   (u32 >>> 10) & 0x3,
     };
 
-    var last = this._last;
     var curt = this._value;
 
     this._edge = {
@@ -140,13 +101,13 @@ function GamePadPlayer_NexusPlayer(u32) { // @arg Uint32
         "Y":    last["Y"]   !== curt["Y"],
         "L1":   last["L1"]  !== curt["L1"],
         "R1":   last["R1"]  !== curt["R1"],
-        "SL":   last["SL"]  !== curt["SL"],
-        "SR":   last["SR"]  !== curt["SR"],
+        "L3":   last["L3"]  !== curt["L3"],
+        "R3":   last["R3"]  !== curt["R3"],
         // --- axis ---         xis ---
-        "SLX":  last["SLX"] !== curt["SLX"],
-        "SLY":  last["SLY"] !== curt["SLY"],
-        "SRX":  last["SRX"] !== curt["SRX"],
-        "SRY":  last["SRY"] !== curt["SRY"],
+        "LSX":  last["LSX"] !== curt["LSX"],
+        "LSY":  last["LSY"] !== curt["LSY"],
+        "RSX":  last["RSX"] !== curt["RSX"],
+        "RSY":  last["RSY"] !== curt["RSY"],
         "L2":   last["L2"]  !== curt["L2"],
         "R2":   last["R2"]  !== curt["R2"],
     };
